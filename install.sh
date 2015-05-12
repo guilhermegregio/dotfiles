d07bda4a8ee8976da68e551090e193664c534766
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2015-05-12 18:57:35 -0300

    ignorando README e LICENSE no install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for f in $(find ~/dotfiles/* -maxdepth 0 -type f -not -name "install.sh")
+for f in $(find ~/dotfiles/* -maxdepth 0 -type f -not -name "install.sh" -not -name "LICENSE" -not -name "README.md")
 do
 	rm -f "$HOME/.${f##*/}"
 	ln -s "$f" "$HOME/.${f##*/}"
