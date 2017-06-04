5bcf4d3af2dc539d2e53dc43fa7d28a44dc1abf7
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2017-06-04 21:00:55 +0000

    teste

--- install.sh
+++ install.sh
@@ -2,36 +2,34 @@
 
 TIMESTAMP=$(date +%s);
 
-if ! type git >/dev/null 2>&1 ; then
-    sudo apt-get install -y git
-    sh ./install.sh
-    exit 0;
-fi
-
-if ! type vim >/dev/null 2>&1 ; then
-    sudo apt-get install -y vim-gtk
-    sh ./install.sh
-    exit 0;
-fi
-sudo apt-get install -y zsh xclip git git-flow tig vim-gtk tmux python-pip curl software-properties-common python-software-properties
-sudo apt-get install -y automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
-pip install git+git://github.com/Lokaltog/powerline 
-pip install awscli
-
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf 
-wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf 
-
-sudo mv PowerlineSymbols.otf /usr/share/fonts/ 
-sudo fc-cache -vf 
-sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
-
-
-
-git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.3.0
+#if ! type git >/dev/null 2>&1 ; then
+#    sudo apt-get install -y git
+#    sh ./install.sh
+#    exit 0;
+#fi
+#
+#if ! type vim >/dev/null 2>&1 ; then
+#    sudo apt-get install -y vim-gtk
+#    sh ./install.sh
+#    exit 0;
+#fi
+#sudo apt-get install -y zsh xclip git git-flow tig vim-gtk tmux python-pip curl software-properties-common python-software-properties
+#sudo apt-get install -y automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
+#pip install git+git://github.com/Lokaltog/powerline 
+#pip install awscli
+#
+#wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf 
+#wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf 
+#
+#sudo mv PowerlineSymbols.otf /usr/share/fonts/ 
+#sudo fc-cache -vf 
+#sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+#
+#
+#
+#git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.3.0
 
 git clone --recursive https://github.com/guilhermegregio/dotfiles.git
-
-sh $HOME/dotfiles/fonts/install.sh
 
 for f in $(find ~/dotfiles/* -maxdepth 0 -type f -not -name "install.sh" -not -name "LICENSE" -not -name "README.md")
 do
