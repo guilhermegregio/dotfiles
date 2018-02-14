b8a5ae1690107a19b0537b5c3c97d1d7111edb8c
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2018-02-14 17:35:23 -0200

    Update scripts

--- npm/install.sh
+++ npm/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if ! which nvm &> /dev/null; then
-    echo "Installing nvm"
+if ! command -v nvm; then
+    echo "Installing Nvm"
     curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 		. $HOME/.nvm/nvm.sh
 fi
+
+echo "Nvm running"
 
 nvm install node
 
--- pip/install.sh
+++ pip/install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-if ! which pip &> /dev/null; then
+if ! command -v pip; then
     echo "Installing Pip"
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python
 fi
+
+echo "Pip running"
 
 pip install --upgrade pip
 # pip install git+git://github.com/Lokaltog/powerline 
