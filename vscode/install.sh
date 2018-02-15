404d6be4bce11c42ac49b0fa44b68f959a90326b
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2018-02-15 19:23:33 -0200

    scripts

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-command -v code || return
+# command -v code || return
 
-dotfilespath="$HOME/dotfiles"
+# dotfilespath="$HOME/dotfiles"
 
-rm -rf ~/Library/Application\ Support/Code/User/snippets
-ln -sf "$dotfilespath/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
-ln -sf "$dotfilespath/vscode/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf "$dotfilespath/vscode/snippets/" ~/Library/Application\ Support/Code/User/snippets
+# rm -rf ~/Library/Application\ Support/Code/User/snippets
+# ln -sf "$dotfilespath/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
+# ln -sf "$dotfilespath/vscode/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
+# ln -sf "$dotfilespath/vscode/snippets/" ~/Library/Application\ Support/Code/User/snippets
