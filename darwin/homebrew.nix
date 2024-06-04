b885b58ea756dc01f2fbb5d62718d0797ece1702
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2024-06-04 15:59:07 -0300

    chore: update

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -4,9 +4,9 @@
     global = { autoUpdate = false; };
     # will not be uninstalled when removed
     masApps = {
-      Xcode = 497799835;
-      Transporter = 1450874784;
-      VN = 1494451650;
+      # Xcode = 497799835;
+      # Transporter = 1450874784;
+      # VN = 1494451650;
     };
     onActivation = {
       # "zap" removes manually installed brews and casks
@@ -15,18 +15,17 @@
       upgrade = false;
     };
     brews = [
-      "borders" # borders for windows
+      # "borders" # borders for windows
     ];
     casks = [
       "wezterm"
       "zoom"
       "slack"
-      "zed" # vim like editor
+      "zed" # editor
     ];
     taps = [
       # default
       "homebrew/bundle"
-      "homebrew/cask-fonts"
       "homebrew/services"
       # custom
       "FelixKratz/formulae" # borders
