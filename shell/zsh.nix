a85b88b0c9be0a4dcdcecf3fbb413df9b1236579
Guilherme Mangabeira Gregio <guilherme@gregio.net>
2024-06-06 18:26:28 -0300

    feat: configure plugin zsh-nix-shell

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -65,13 +65,23 @@
     };
 
     plugins = [
+      # {
+      #   name = "zsh-nix-shell";
+      #   src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
+      # }
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
       {
         name = "fast-syntax-highlighting";
         src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
-      }
-      {
-        name = "zsh-nix-shell";
-        src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
       }
       {
         name = "forgit";
